7746d9392de74ff81c901e71cd9aa2039f2590b5
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-06-09 21:07:43 +0200

    fixes helpers import

--- api/service.go
+++ api/service.go
@@ -6,10 +6,10 @@ import (
 
 	"net/http"
 
-	"github.com/Seklfreak/Robyul2/helpers"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/render"
+	"gitlab.com/Cacophony/dhelpers"
 	"gitlab.com/Cacophony/dhelpers/apihelper"
 	"gitlab.com/Cacophony/dhelpers/cache"
 )
@@ -45,5 +45,5 @@ func getStats(w http.ResponseWriter, r *http.Request) {
 
 	// return result
 	err := render.Render(w, r, result)
-	helpers.RelaxLog(err)
+	dhelpers.LogError(err)
 }
