de63d902a363592f80612f1fd1461351661d0a3e
Sebastian Winkler <sebastian.winkler@slmn.de>
2019-10-15 20:14:07 +0200

    eventlog-auditlog: improves queries

--- plugins/eventlog-auditlog/repo.go
+++ plugins/eventlog-auditlog/repo.go
@@ -17,12 +17,12 @@ func setAuthor(ctx context.Context, tx *sql.Tx, entryID uint, authorID string) e
 
 	query := `
 UPDATE eventlog_items
-SET author_id = $1
+SET author_id = $1, updated_at = $3
 WHERE id = $2
 ;
 `
 
-	_, err := tx.ExecContext(ctx, query, authorID, entryID)
+	_, err := tx.ExecContext(ctx, query, authorID, entryID, time.Now().UTC())
 	return err
 }
 
@@ -40,11 +40,16 @@ func addItemOption(ctx context.Context, tx *sql.Tx, entryID uint, key, previousV
 
 	query := `
 INSERT INTO eventlog_item_options ("created_at", "updated_at", "item_id", "key", "previous_value", "new_value", "type", "author_id")
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+VALUES ($1, $1, $2, $3, $4, $5, $6, $7)
+ON CONFLICT ("author_id", "key")
+DO UPDATE
+SET "updated_at" = $1
+,   "previous_value" = $4
+,   "new_value" = $5
+,   "type" = $6
 ;
 `
 
-	now := time.Now().UTC()
-	_, err := tx.ExecContext(ctx, query, now, now, entryID, key, previousValue, newValue, optionType, botID)
+	_, err := tx.ExecContext(ctx, query, time.Now().UTC(), entryID, key, previousValue, newValue, optionType, botID)
 	return err
 }
